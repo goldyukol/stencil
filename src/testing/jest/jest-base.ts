0a94a0242c361c7eb283c3084d6f15d73bb05bd1
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-09-29 15:00:50 -0400

    abstract

--- src/testing/jest/jest-base.ts
+++ src/testing/jest/jest-base.ts
@@ -3,38 +3,24 @@ import type * as d from '@stencil/core/internal';
 import { JestPreprocessor, JestPuppeteerEnvironment, JestTestRunner } from './jest-apis';
 
 export abstract class JestAdapter {
-  getRunner(): (config: d.ValidatedConfig, e2eEnv: d.E2EProcessEnv) => Promise<boolean> {
-    throw new Error('TODO');
-  }
-  getScreenshot(): (config: d.ValidatedConfig, e2eEnv: d.E2EProcessEnv) => Promise<boolean> {
-    throw new Error('TODO');
-  }
+  abstract getRunner(): (config: d.ValidatedConfig, e2eEnv: d.E2EProcessEnv) => Promise<boolean>;
+  abstract getScreenshot(): (config: d.ValidatedConfig, e2eEnv: d.E2EProcessEnv) => Promise<boolean>;
 
   /**
    * Retrieve the default Jest runner name prescribed by Stencil
    * @returns the stringified name of the test runner, based on the currently detected version of Stencil
    */
-  getDefaultJestRunner(): string {
-    return 'jest-jasmine2';
-  }
+  abstract getDefaultJestRunner(): string;
 
   getJestModuleNames = (): string[] => {
     return ['@types/jest', 'jest', 'jest-cli'];
   };
 
-  getCreateJestPuppeteerEnvironment(): () => JestPuppeteerEnvironment {
-    throw new Error('TODO');
-  }
+  abstract getCreateJestPuppeteerEnvironment(): () => JestPuppeteerEnvironment;
 
-  getJestPreprocessor(): JestPreprocessor {
-    throw new Error('TODO');
-  }
+  abstract getJestPreprocessor(): JestPreprocessor;
 
-  getCreateJestTestRunner(): JestTestRunner {
-    throw new Error('TODO');
-  }
+  abstract getCreateJestTestRunner(): JestTestRunner;
 
-  getJestSetupTestFramework(): void {
-    throw new Error('TODO');
-  }
+  abstract getJestSetupTestFramework(): void;
 }

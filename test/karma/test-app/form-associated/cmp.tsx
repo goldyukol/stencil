1e3101d582bbbe182378489bab75ae50b47254e4
Alice Pote <alice.writes.wrongs@gmail.com>
2023-10-06 15:16:46 -0400

    remove unused stuff (for now at least)

--- test/karma/test-app/form-associated/cmp.tsx
+++ test/karma/test-app/form-associated/cmp.tsx
@@ -8,16 +8,11 @@ export class FormAssociatedCmp {
   @AttachInternals()
   internals: ElementInternals;
 
-  handleChange(event: any) {
-    console.log('Ive been called!');
-    this.internals.setFormValue(event.target.value);
-  }
-
   componentWillLoad() {
     this.internals.setFormValue('my default value');
   }
 
   render() {
-    return <input type="text" onInput={(event) => this.handleChange(event)} />;
+    return <input type="text" />;
   }
 }

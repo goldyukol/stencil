be68fbef7102b0c40e2a4aef98dec1e2cac99f8f
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-10-20 16:13:47 -0400

    more processor work, is this better?

--- src/testing/jest/jest-apis.ts
+++ src/testing/jest/jest-apis.ts
@@ -21,10 +21,23 @@ import { Jest28TransformOptions } from "./jest-28/jest-preprocessor";
 // TODO(STENCIL-959): Improve this typing by narrowing it
 export type JestPuppeteerEnvironment = any;
 
-type Jest26CacheKeyOptions = { instrument: boolean; rootDir: string };
 type Jest26Config = { instrument: boolean; rootDir: string };
 type Jest27TransformOptions = { config: Jest26Config };
-type Jest27Preprocessor = {
+
+type BasePreprocessor = {
+  process(
+    sourceText: string,
+    sourcePath: string,
+    ...args: any[]
+  ): string | TransformedSource;
+  getCacheKey(
+    sourceText: string,
+    sourcePath: string,
+    ...args: any[]
+  ): string;
+};
+
+type Jest27Preprocessor = BasePreprocessor & {
   process(
     sourceText: string,
     sourcePath: string,
@@ -35,22 +48,25 @@ type Jest27Preprocessor = {
     sourceText: string,
     sourcePath: string,
     jestConfigStr: string | Jest27TransformOptions,
-    transformOptions?: Jest26CacheKeyOptions,
+    transformOptions?: Jest26Config,
   ): string;
 };
-type Jest28Preprocessor = {
+
+type Jest28Preprocessor = BasePreprocessor & {
   process(
     sourceText: string,
     sourcePath: string,
-    options: Jest28TransformOptions, // TODO(NOW)
-  ): string | TransformedSource;
+    options: Jest28TransformOptions,
+  ): TransformedSource;
   getCacheKey(
     sourceText: string,
     sourcePath: string,
-    options: Jest28TransformOptions, // TODO(NOW)
+    options: Jest28TransformOptions,
   ): string;
 };
+
 export type JestPreprocessor = Jest27Preprocessor | Jest28Preprocessor;
+
 
 // TODO(STENCIL-960): Improve this typing by narrowing it
 export type JestTestRunner = any;
